294185e0ded1d475049c329bda73eb296e700540
Scapter <slyhades66@gmail.com>
2024-03-09 16:55:01 -0500

    Update ScapLib.lua

--- ScapLib.lua
+++ ScapLib.lua
@@ -30,13 +30,13 @@ local CurrentPage	= 0
 
 local function GetUIParent()
 	local Success	= pcall(function() return CoreGui.Name end)
-	
+
 	return not Success and Player:WaitForChild("PlayerGui") or CoreGui
 end
 
 local function ChangePage(PageOffset: number, Tab: TextButton)
 	UI.Base.Pages.Slide:TweenPosition(UDim2.new(PageOffset * -1, 0, 0, 0), "Out", "Linear", 0.2, true)
-	
+
 	for i,v in TabTweens do
 		if i == Tab then
 			v.Closed:Pause()
@@ -54,12 +54,12 @@ end
 UI.Base.Ignore.Drag.MouseButton1Down:Connect(function()
 	local StartingPos	= UserInputService:GetMouseLocation()
 	local UIStart	= UI.Base.Position
-	
+
 	task.wait(0.375)
 	while RunService.RenderStepped:Wait() and UserInputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) do
 		local NewPosition	= UserInputService:GetMouseLocation()
 		local DragOffset	= (NewPosition-StartingPos)
-		
+
 		UI.Base:TweenPosition(UIStart + UDim2.new(0, DragOffset.X, 0, DragOffset.Y), "Out", "Linear", 0.01, true)
 	end
 end)
@@ -67,7 +67,7 @@ end)
 function Library:CreatePage(PageName: string)
 	local MyPage	= PageCount
 	local NewPage	= {}
-	
+
 	--	// Building Visuals
 	local TabVisual	= Tab:Clone()
 	TabVisual.LayoutOrder	= MyPage
@@ -76,10 +76,10 @@ function Library:CreatePage(PageName: string)
 	TabVisual.Highlight.Size	= UDim2.new(not FirstPage and 0.6 or 0, 0, 0.05, 0)
 	TabVisual.Text	= PageName
 	TabVisual.Parent	= UI.Base.Tabs
-	
+
 	local PageVisual	= Page:Clone()
 	PageVisual.Parent	= UI.Base.Pages.Slide
-	
+
 	TabTweens[TabVisual]	= {
 		Opened	= TweenService:Create(TabVisual.Highlight, TweenInfo.new(0.2, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {
 			BackgroundColor3	= Color3.fromRGB(85, 255, 127)
@@ -88,7 +88,7 @@ function Library:CreatePage(PageName: string)
 			BackgroundColor3	= Color3.fromRGB(85, 170, 255)
 		}),
 	}
-	
+
 	--	// Handle Tab Visual Effects
 	TabVisual.MouseEnter:Connect(function()
 		TabVisual.Highlight:TweenSize(UDim2.new(MyPage == CurrentPage and 0.7 or 0.6, 0, 0.05, 0), "Out", "Sine", 0.2, true)
@@ -96,91 +96,92 @@ function Library:CreatePage(PageName: string)
 	TabVisual.MouseLeave:Connect(function()
 		TabVisual.Highlight:TweenSize(UDim2.new(MyPage == CurrentPage and 0.5 or 0, 0, 0.05, 0), "Out", "Sine", 0.2, true)
 	end)
-	
+
 	TabVisual.MouseButton1Down:Connect(function()
 		CurrentPage	= MyPage
-		
+
 		ChangePage(MyPage, TabVisual)
 	end)
-	
+
 	if not FirstPage then
 		FirstPage	= true
-		
+
 		local RealSize = UI.AbsoluteSize
 		UI.Base.Position = UDim2.new(0, RealSize.X/2, 0, RealSize.Y/2)
 		UI.Parent	= GetUIParent()
 	end
-	
+
 	--	// Add Dividers For Sub-Categories
 	NewPage.AddDivider	= function()
 		local DividerVisual	= Assets.Divider:Clone()
 		DividerVisual.Parent	= PageVisual
 	end
-	
+
 	NewPage.CreateLabel = function(Description: string)
 		local Callback	= {}
-		
+
 		local NewLabel = Assets.Label:Clone()
 		NewLabel.Description.Text = Description
 		NewLabel.Parent= PageVisual
-		
+
 		Callback.Update = function(NewDescription: string)
 			NewLabel.Description.Text	= NewDescription
 		end
-		
+
 		return Callback
 	end
-	
+
 	--	// Handle Keybind Inputs
 	NewPage.CreateKeybind	= function(Description: string, Key: Enum.KeyCode, Callback: any)
 		local KeybindVisual	= Keybind:Clone()
 		KeybindVisual.Description.Text	= Description
 		KeybindVisual.Click.Text	= tostring(Key):gsub("Enum.KeyCode.", "")
 		KeybindVisual.Parent	= PageVisual
-		
+
 		KeybindVisual.Click.MouseButton1Down:Connect(function()
 			KeybindVisual.Click.Text	= "..."
-			
+
 			local WaitForInput
 			WaitForInput	= UserInputService.InputBegan:Connect(function(Input: InputObject)
 				if Input and Input.KeyCode and Input.KeyCode ~= Enum.KeyCode.Unknown then
 					Key	= Input.KeyCode
 					KeybindVisual.Click.Text	= tostring(Key):gsub("Enum.KeyCode.", "")
-					
+
 					if Callback then
 						task.wait(0.1)
-						
+
 						Callback(Key)
 					end
-					
+
 					WaitForInput:Disconnect()
 				end
 			end)
 		end)
 	end
-	
+
 	--	// Handle Dropdowns
 	NewPage.CreateDropdown	= function(Description: string, Selected: string, Entries: table, Callback: any)
 		local IsOpen	= false
 		local isClosing	= false
 		local DropdownSendback	= {}
 		local EntryButtons	= {}
-		
+
 		local DropdownVisual	= Dropdown:Clone()
 		DropdownVisual.Click.Text	= Selected
 		DropdownVisual.Description.Text	= Description
 		DropdownVisual.Parent	= PageVisual
-		
+
 		local ArrowOpen	= TweenService:Create(DropdownVisual.Click.Drop, TweenInfo.new(0.1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {
 			Rotation	= 180,
 		})
 		local ArrowClose	= TweenService:Create(DropdownVisual.Click.Drop, TweenInfo.new(0.1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {
 			Rotation	= 0,
 		})
-		
+
 		local function Close()
+			DropdownVisual.ZIndex = 10
 			IsOpen	= false
-			
+
 			if not isClosing	 then
 				isClosing	= true
 				DropdownVisual.Click.List:TweenSize(UDim2.new(0.9, 0, 0, 0), "Out", "Linear", 0.1, true)
@@ -195,7 +196,7 @@ function Library:CreatePage(PageName: string)
 			ArrowOpen:Pause()
 			ArrowClose:Play()
 		end
-		
+
 		local function CreateEntry(Entry: string)
 			local NewEntry	= Assets.Entry:Clone()
 			NewEntry.Text	= Entry
@@ -207,44 +208,45 @@ function Library:CreatePage(PageName: string)
 				Callback(Entry)
 				Close()
 			end)
-			
+
 			EntryButtons[Entry]	= NewEntry
 		end
-		
+
 		for _,v in Entries do
 			CreateEntry(v)
 		end
-		
+
 		DropdownVisual.Click.MouseButton1Down:Connect(function()
 			IsOpen	= not IsOpen
-			
+
 			if IsOpen then
+				DropdownVisual.ZIndex = 12
 				DropdownVisual.Click.List.Visible	= true
 				DropdownVisual.Click.List:TweenSize(UDim2.new(0.9, 0, 9, 0), "Out", "Linear", 0.1, true)
-				
+
 				ArrowClose:Pause()
 				ArrowOpen:Play()
 			else
 				Close()
 			end
 		end)
-		
+
 		function DropdownSendback:NewEntry(Entry: string)
 			if not table.find(EntryButtons, Entry) then
 				CreateEntry(Entry)
 			end
 		end
-		
+
 		function DropdownSendback:RemoveEntry(Entry: string)
 			if EntryButtons[Entry] then
 				EntryButtons[Entry]:Destroy()
 				EntryButtons[Entry]	= nil
 			end
 		end
-		
+
 		return DropdownSendback
 	end	
-		
+
 	--	// Handle Sliders
 	NewPage.CreateSlider	= function(Description: string, Start: number, Minimum: number, Maximum: number, Callback: any)
 		local SliderVisual	= Slider:Clone()
@@ -260,21 +262,21 @@ function Library:CreatePage(PageName: string)
 				local NewX	= math.clamp(RelativeUI.X, 0, SliderVisual.Slider.AbsoluteSize.X)
 				local ToScale	= NewX/SliderVisual.Slider.AbsoluteSize.X
 				Start	= math.clamp(math.floor(Maximum*ToScale), Minimum, Maximum)
-				
+
 				SliderVisual.Slider.Progress.Text	= Start.."/"..Maximum
 				SliderVisual.Slider.Filler:TweenSize(UDim2.new(ToScale, 0, 0.85, 0), "Out", "Linear", 0.1, true)
-				
+
 				if Callback then
 					Callback(Start)
 				end
 			end
 		end
-		
+
 		SliderVisual.Slider.MouseButton1Down:Connect(function()
 			doSlide()
 		end)
 	end
-	
+
 	--	// Handle InputBoxes
 	NewPage.CreateInput	= function(Description: string, Default: string, Callback: any)
 		local InputVisual	= Input:Clone()
@@ -288,21 +290,21 @@ function Library:CreatePage(PageName: string)
 			end
 		end)
 	end
-	
+
 	--	// Handle Click Buttons
 	NewPage.CreateButton	= function(Description: string, Callback: any)
 		local NewButton	= Button:Clone()
 		NewButton.Description.Text	= Description
-		
+
 		NewButton.Click.MouseButton1Down:Connect(function()
 			if Callback then
 				Callback()
 			end
 		end)
-		
+
 		NewButton.Parent	= PageVisual
 	end
-	
+
 	--	// Handle Toggle Buttons
 	NewPage.CreateToggle	= function(Description: string, Status: boolean, Callback: any)
 		local ToggleVisual	= Toggle:Clone()
@@ -316,7 +318,7 @@ function Library:CreatePage(PageName: string)
 		local OffTween	= TweenService:Create(ToggleVisual.Click.UIStroke, TweenInfo.new(0.2, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {
 			Color	= Color3.fromRGB(255, 85, 127)
 		})
-		
+
 		local OnGradientTween	= TweenService:Create(ToggleVisual.Toggled.UIGradient, TweenInfo.new(0.2, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {
 			Offset	= Vector2.new(0, 0)
 		})
@@ -329,13 +331,13 @@ function Library:CreatePage(PageName: string)
 			if StateChange.Value then
 				OffTween:Pause()
 				OffGradientTween:Pause()
-				
+
 				OnTween:Play()
 				OnGradientTween:Play()
 			else
 				OnTween:Pause()
 				OnGradientTween:Pause()
-				
+
 				OffTween:Play()
 				OffGradientTween:Play()
 			end
@@ -353,7 +355,7 @@ function Library:CreatePage(PageName: string)
 		ToggleVisual.Parent	= PageVisual
 		StateChange.Value	= Status
 	end
-	
+
 	PageCount	+= 1
 	return NewPage
 end
